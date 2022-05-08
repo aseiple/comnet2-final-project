34357655e80a3522e0de8b2b3f8803a2aaeb0d9c
August Seiple <aseiple678@gmail.com>
2022-05-08 12:17:09 -0400

    Update packet actions

--- router.py
+++ router.py
@@ -30,20 +30,38 @@ class udprouter:
             pkt_type = packet[1]
             if pkt_type == 1:
                 seq, ttl, src = read_hello(packet)
+                # Update routing table with new destination and forward HELLO packet
+                # to neighbors
             elif pkt_type == 2:
                 seq, src, n, k, ln, dests = read_data_multicast_header(packet)
                 data = read_data_multicast_data(packet)
+                # Initiate centroid finding algorithm by transmitting
+                # CENTROID_REQUEST to all neighbors
             elif pkt_type == 3:
                 seq, src, n, dests = read_centroid_request(packet)
+                # Respond with CENTROID_REPLY packet to sending router with data
+                # containing mean distance to specified N destination nodes
             elif pkt_type == 4:
                 seq, src, mean_dist = read_centroid_reply(packet)
+                #  Compare own mean distance to those in the packet and forward new
+                # DATA_MULTICAST packet to minimum cost. If self, forward
+                # DATA_UNICAST packets to K closest destinations out of N specified
+                # and transmit DATA_MULTICAST_ACK to original sender node
             elif pkt_type == 5:
                 seq, src, srccentroid, dst = read_data_unicast_header(packet)
                 data = read_data_unicast_data(packet)
+                # Forward packet to next hop toward node destination specified. If
+                # destination node, transmit DATA_UNICAST_ACK to centroid
+                # destination specified
             elif pkt_type == 6:
                 seq, src, dst = read_data_multicast_ack(packet)
+                # Forward packet to next hop toward original sender node destination
+                # specified
             elif pkt_type == 7:
                 seq, src, dst, dstcentroid = read_data_unicast_ack(packet)
+                # Forward packet to next hop toward centroid destination specified. If
+                # centroid, wait for K such packets and transmit
+                # DATA_MULTICAST_ACK toward original sender node
             else:
                 print('Unknown packet type')
 
